8943116c68cc9a8d6fb549c8036e288639234d8e
Yu Yanglong <harpsword@163.com>
2015-06-11 20:31:42 +0800

    include

---
+++ include/InitOpenGL.h
@@ -0,0 +1,14 @@
+
+#ifndef __INIT_OPENGL_H__
+#define __INIT_OPENGL_H__
+
+#define BUFFER_OFFSET(offset) ((void *)(offset))
+
+#include<GL\glew.h>
+#pragma comment (lib,"glew32s.lib")
+
+//#define GLUT_DISABLE_ATEXIT_HACK
+#include<GL\glut.h>
+
+#endif
+
